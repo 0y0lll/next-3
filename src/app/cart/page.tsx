e120b72cc271772ae537057c8c48ffd686f694c9
oyo <bemyangel2@gmail.com>
2024-06-23 16:52:16 +0900

    feat: map

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,6 +6,13 @@ export default function Cart() {
 			<h4 className="title" style={{ fontSize: 30 }}>
 				Cart
 			</h4>
+
+			<div className="cart-item">
+				<p>상품명</p>
+				<p>$40</p>
+				<p>1개</p>
+			</div>
+
 			<Link href="/cart/payment">Payment</Link>
 		</div>
 	)
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,16 +1,43 @@
+import Image from "next/image"
+
 export default function List() {
+	const products = [
+		{
+			name: "상품1",
+			price: 40,
+			imgSrc: "/food0.png",
+		},
+		{
+			name: "상품2",
+			price: 30,
+			imgSrc: "/food1.png",
+		},
+		{
+			name: "상품3",
+			price: 50,
+			imgSrc: "/food2.png",
+		},
+	]
+
 	return (
 		<div>
 			<h4 className="title" style={{ fontSize: 30 }}>
 				List
 			</h4>
 
-			<div className="food">
-				<h4>상품1 $40</h4>
-			</div>
-			<div className="food">
-				<h4>상품2 $40</h4>
-			</div>
+			{products.map((item, index) => (
+				<div key={index} className="food">
+					<Image
+						src={item.imgSrc}
+						alt={item.name}
+						width={400}
+						height={400}
+					/>
+					<h4>
+						{item.name} ${item.price}
+					</h4>
+				</div>
+			))}
 		</div>
 	)
 }
